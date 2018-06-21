a203cfd5601ef0167249c2542facf6014628bc56
abc <2743630636@qq.com>
2018-06-21 14:54:20 +0800

    123

---
+++ helloclass.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef HELLOCLASS_H
+#define HELLOCLASS_H
+#include<iostream>
+using namespace std;
+class HelloClass
+{
+public:
+	HelloClass();
+	void sayHello();
+	~HelloClass();
+};
+#endif // !HelloClass.h
